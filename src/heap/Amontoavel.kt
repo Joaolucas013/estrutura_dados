c18644b167b14ae2479a43f3e203767fae1f71a3
joaolucas013 <joaoolucass2001@gmail.com>
2024-11-24 11:20:11 -0300

    Movendo Amontoavel.kt e HeapMin.kt para a pasta heap

---
+++ src/heap/Amontoavel.kt
@@ -0,0 +1,16 @@
+package heap
+
+interface Amontoavel {
+
+
+    fun inserir(dado:Long)
+    fun obter():Long?
+    fun atualizar(novoDado:Long)
+    fun extrair():Long
+
+    fun estaCheia():Boolean
+    fun estaVazia():Boolean
+    fun imprimir():String
+
+
+}
