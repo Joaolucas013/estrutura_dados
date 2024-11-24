a294ef5b97fb798b5508244fcf22b9df2f9b538c
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2024-11-24 15:29:31 +0000

    atualizando...

--- src/heap/Amontoavel.kt
+++ src/heap/Amontoavel.kt
@@ -1,5 +1,21 @@
 package heap
 
+<<<<<<< HEAD
+interface Amontoavel {
+
+
+    fun inserir(dado:Long)
+    fun obter():Long?
+    fun atualizar(novoDado:Long)
+    fun remover():Long
+
+    fun estaCheia():Boolean
+    fun estaVazia():Boolean
+    fun imprimir():String
+
+
+}
+=======
 interface Amontoavel {
 
 
@@ -14,3 +30,4 @@ interface Amontoavel {
 
 
 }
+>>>>>>> 4277b334669dd970eb02da0e4f9011a20470e976
