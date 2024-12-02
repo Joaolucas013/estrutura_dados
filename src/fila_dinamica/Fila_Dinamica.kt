f292ca9d7993e23cbcb1ae14a11d639f12997b40
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2024-12-02 00:21:13 -0300

    atualizando

--- src/fila_dinamica/Fila_Dinamica.kt
+++ src/fila_dinamica/Fila_Dinamica.kt
@@ -7,50 +7,76 @@ class Fila_Dinamica(var tamanho:Int = 10):EnfileiravelDinamica {
     var quantidade: Int = 0
 
 
-//
-
-    override fun enfileirar(dado: Any?) {
-
-
-    }
+override fun enfileirar(dado: Any?) {
+		if (!estaCheia()) {
+			var noTemp = NoDuplo(dado)
+			//noTemp.dado = dado
+			noTemp.anterior = ponteiroFim
+			if (!estaVazia())
+				ponteiroFim?.proximo = noTemp
+			else
+				ponteiroInicio = noTemp
+				
+			ponteiroFim = noTemp
+			quantidade = quantidade.inc()
+		} else {
+			println("Fila Cheia!")
+		}
+	}
+	
+	override fun desenfileirar(): Any? {
+		var dadoAux: Any? = null
+		if (!estaVazia()) {
+			dadoAux = ponteiroInicio?.dado
+			ponteiroInicio = ponteiroInicio?.proximo
+			quantidade = quantidade.dec()
+			if (!estaVazia())
+				ponteiroInicio?.anterior = null
+			else
+				ponteiroFim = null
+		} else {
+			println("Fila Vazia!")
+		}
+		return dadoAux
+	}
+	
+	override fun frente(): Any? {
+		var dadoAux: Any? = null
+		if (!estaVazia())
+			dadoAux = ponteiroInicio?.dado
+		else
+			println("Fila Vazia!")
+		
+		return dadoAux
+	}
 
     override fun atualizar(dado: Any?) {
+		if (!estaVazia())
+			ponteiroInicio?.dado = dado 
+		else
+			println("Fila Vazia!")
 
-    }
+	}	
 
-    override fun frente(): Any? {
-        var aux:Any? = null
-        if(!estaVazia()){
-            aux = ponteiroInicio?.dado
-        } else{
-            println("Fila esta vazia!!!")
-        }
-        return aux
-    }
-
-    override fun desenfileirar(): Any? {
-       var aux:Any? = null
-        if(!estaVazia()){
-            aux = ponteiroInicio?.dado
-            if(ponteiroInicio!=null){
-                ponteiroInicio?.anterior= null
-            } else{
-                ponteiroFim
-            }
-        }
-
-    }
-
-    override fun estaCheia(): Boolean {
-      return quantidade == tamanho
-    }
-
-    override fun estaVazia(): Boolean {
-       return quantidade==0
-    }
-
-    override fun imprimir(): String {
-
-    }
+	override fun estaCheia(): Boolean {
+		return quantidade == tamanho
+	}
+	
+	override fun estaVazia(): Boolean {
+		return quantidade == 0
+	}
+	
+	override fun imprimir(): String {
+		var ponteiroAuxiliar = ponteiroInicio
+		var resultado = "["
+		for (i in 0 until quantidade) {
+			resultado += "${ponteiroAuxiliar?.dado}"
+			if (i != quantidade-1)
+				resultado += ","
+			
+			ponteiroAuxiliar = ponteiroAuxiliar?.proximo
+		}
+		return "$resultado]"
+	}
 
 }
