ee895ab515db2b5939eea51a6c36acb60e26c01c
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2024-12-07 01:07:30 -0300

    Update HeapMin.kt

--- src/heap/HeapMin.kt
+++ src/heap/HeapMin.kt
@@ -4,20 +4,21 @@ package heap
 class HeapMin(private val tamanho:Int = 10):Amontoavel {
 
     private var dados = LongArray(tamanho) { 0 }
-    private var ponteiroFim: Int = -1
+    private var ponteiroFim:Int = -1
+    private var quantidade:Int = 0
 
- override fun inserir(dado: Long) {
+    override fun inserir(dado: Long) {
         if (!estaCheia()) {
             ponteiroFim++
             dados[ponteiroFim] = dado
             ajustarAcima(ponteiroFim)
+            quantidade++
         } else {
             println("Heap esta cheia!!!")
         }
-}
+    }
 
-
-    private fun ajustarAcima(indice: Int) {
+    override fun ajustarAcima(indice: Int) {
         var indiceFilho = indice
         var indicePai = pai(indiceFilho)
 
@@ -25,59 +26,66 @@ class HeapMin(private val tamanho:Int = 10):Amontoavel {
             if (dados[indicePai] > dados[indiceFilho]) {
                 troca(indiceFilho, indicePai)
                 indiceFilho = indicePai
-                indicePai = indiceFilho
+                indiceFilho = pai(indiceFilho)
             } else {
                 break
             }
         }
     }
 
-    private fun ajustarAbaixoRec(indice: Int) {
-        val pai = indice
-        val filhoEsquerdo = indiceFilhoEsquerda(pai)
-        val filhoDireito = indiceFilhoDireita(pai)
-        var menor = pai;   // inicialmente, o pai é o menor
-    
-        if (filhoEsquerdo <= ponteiroFim) 
-            if (dados[menor] > dados[filhoEsquerdo])
-                menor = filhoEsquerdo
-    
-        if (filhoDireito <= ponteiroFim) 
-            if (dados[menor] > dados[filhoDireito])  
-                menor = filhoDireito
-                
-      
-        if (menor != pai) {
-            trocar(pai, menor)
-            ajustarAbaixoRec(menor)
+    override fun ajustarAbaixo(indice: Int) {
+        var pai = indice
+
+        while (pai <= ponteiroFim) {
+            var filhoEsquerdo = indiceFilhoEsquerdo(pai)
+            var filhoDireito = indiceFilhoDireito(pai)
+            var menorNumero = pai
+
+            if (filhoEsquerdo <= ponteiroFim) {
+                if (dados[filhoEsquerdo] < dados[menorNumero]) {
+                    menorNumero = filhoEsquerdo
+                }
+            }
+            if (filhoDireito <= ponteiroFim) {
+                if (dados[filhoDireito] < dados[menorNumero]) {
+                    menorNumero = filhoDireito
+                }
+            }
+
+            if (menorNumero != pai) {
+                troca(menorNumero, pai)
+                pai = menorNumero
+            } else {
+                break;
+            }
         }
+
     }
 
-
     override fun obter(): Long? {
-        var aux: Long? = null
-        if (!estaVazia()) {
+       var aux:Long? = null
+        if(!estaVazia()){
             aux = dados[0]
-        } else {
-            println("Heap está cheio!!!")
+        } else{
+            println("Heap está vazio!!!")
         }
         return aux
     }
 
-private fun troca(indice1: Int, indice2: Int) {
-    val temp = dados[indice1]
-    dados[indice1] = dados[indice2]
-    dados[indice2] = temp
-}
+    override fun troca(indiceUm: Int, indiceDois: Int) {  // indiceUm = 3, Indice2 = 5
+        var aux = dados[indiceUm]   // aux = 3
+        dados[indiceUm] = dados[indiceDois]   // indiceUM = 5
+        dados[indiceDois] = aux               // IndiceDois = 3
+    }
+
 
     override fun atualizar(novoDado: Long) {
         if(!estaVazia()){
             dados[0] = novoDado
             ajustarAbaixo(0)
         } else{
-            println("Heap esta vazia!!!")
+            println("Heap esta vazio")
         }
-
     }
 
     override fun remover(): Long? {
@@ -86,44 +94,47 @@ private fun troca(indice1: Int, indice2: Int) {
             aux = dados[0]
             dados[0] = dados[ponteiroFim]
             ponteiroFim--
+            quantidade--
             ajustarAbaixo(0)
         } else {
-            println("Heap esta vazia!!!")
+            println("Heap está vazio!!!")
         }
-        return aux
 
+        return aux
     }
 
     override fun estaCheia(): Boolean {
-        return ponteiroFim == dados.size - 1
-
+        return ponteiroFim == tamanho
     }
 
     override fun estaVazia(): Boolean {
-        return ponteiroFim == -1
-
+       return ponteiroFim==-1
     }
+
+
 
     override fun imprimir(): String {
         var resultado = "["
-        for (i in 0 until dados.size) {
+        for (i in 0 until quantidade) {
             resultado += "${dados[i]}"
+            if (i < quantidade - 1){
+                resultado += ", "
+            }
         }
         return "$resultado]"
-
     }
 
-    
-    private fun pai(indiceFilho: Int): Int {
+
+    override fun pai(indiceFilho: Int): Int {
         return (indiceFilho-1)/2
     }
 
-    private fun filhoEsquerdo(indicePai: Int): Int {
-        return (indicePai * 2 + 1)
+    override fun indiceFilhoEsquerdo(indicePai: Int): Int { // 1  2  3  4  5 filho esquerdo de 1 = 0*2 + 1 filho = 2
+        return (indicePai*2) +1                           // 0  1  2  3  4
     }
 
-    private fun filhoDireito(indicePai: Int): Int {
-        return (indicePai * 2 + 2)
+    override fun indiceFilhoDireito(indicePai: Int): Int { // 1  2  3  4  5 (filho direito de 1 = 0*2 + 2 - filho= 3
+        return (indicePai*2) + 2
     }
 
 }
