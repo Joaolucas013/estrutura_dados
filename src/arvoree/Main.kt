ea64f1a2b1f8b1f4ec0f56600e7c4d9590ca1dde
João Lucas <164104520+Joaolucas013@users.noreply.github.com>
2025-02-13 09:04:28 -0300

    Update Main.kt

--- src/arvoree/Main.kt
+++ src/arvoree/Main.kt
@@ -1,3 +1,13 @@
 fun main() {
 
+    var arvor = Arvore()
+    arvor.inserir(10)
+    arvor.inserir(11)
+    arvor.inserir(12)
+    arvor.inserir(13)
+    arvor.inserir(14)
+    print(arvor.buscar(12))
+    print(arvor.buscar(10))
+    arvor.apagar(10)
+    println(arvor.imprimirEmOrdem())
 }
